0b1fd539bd10a0c79afa57f575eed66297aa0cda
José Malcher Jr <contato@josemalcher.net>
2022-06-17 15:55:29 -0300

    07 Executando Primeiros Testes

--- proj01/tests/ProdutoTest.php
+++ proj01/tests/ProdutoTest.php
@@ -2,7 +2,16 @@
 
 namespace Code;
 
-class ProdutoTest
-{
+use PHPUnit\Framework\TestCase;
 
+class ProdutoTest extends TestCase
+{
+    public function testSeONomeDoProdutoESetadoCorretamente()
+    {
+        $produto = new Produto();
+        $produto->setName('Produto 1');
+
+        $this->assertEquals('Produto 2', $produto->getName(), 'Valores não são iguais');
+
+    }
 }
